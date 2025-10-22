9b66b4473a8dfaeae37582e4b5e56b68dc5bc0ff
Doidoria <th15926@daum.net>
2025-10-22 15:15:12 +0900

    .

---
+++ index.js
@@ -0,0 +1,14 @@
+//01
+// const _= require("lodash")
+
+// const arr = [1, 2, 3, 4, 5]
+// const shuffled = _.shuffle(arr)
+// console.log("Shuffled:", shuffled)
+// console.log("HelloWorld!")
+
+//02
+import _ from 'lodash'
+const arr = [1,2,3,4,5]
+const shuffled = _.shuffle(arr)
+console.log("Shuffled_2:", shuffled)
+console.log("HelloWorld!!!!")
